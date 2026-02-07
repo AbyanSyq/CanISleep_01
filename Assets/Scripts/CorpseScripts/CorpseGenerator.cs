fbdac52086a42227117d2a96bf1abeb05901315e
Abyan Syauqi <m.abyan.syauqi@gmail.com>
2026-02-07 21:39:00 +0700

    Fix : corpse and respawn system

--- Assets/Scripts/CorpseScripts/CorpseGenerator.cs
+++ Assets/Scripts/CorpseScripts/CorpseGenerator.cs
@@ -1,12 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CorpseGenerator : SingletonMonoBehaviour<CorpseGenerator>
 {
     [SerializeField] private GameObject corpsePrefab;
+    [SerializeField] private int poolSize = 5;
+    private Queue<GameObject> corpsePool = new Queue<GameObject>();
 
+    protected override void Awake()
+    {
+        base.Awake();
+        InitializePool();
+    }
+
+    void OnEnable()
+    {
+        GameplayManager.Instance.OnGameplayRestart.AddListener(GeneratePlayerCorpse);
+    }
+    void OnDisable()
+    {
+        GameplayManager.Instance.OnGameplayRestart.RemoveListener(GeneratePlayerCorpse);
+    }
+
+    private void InitializePool()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(corpsePrefab);
+            obj.SetActive(false);
+            corpsePool.Enqueue(obj);
+        }
+    }
+    public void GeneratePlayerCorpse()
+    {
+        GenerateCorpse(GameplayManager.Instance.playerInputHandler.transform.position); 
+    }
     public void GenerateCorpse(Vector3 position)
     {
-        Instantiate(corpsePrefab, position, corpsePrefab.transform.rotation);
-        Debug.Log("Corpse generated at position: " + position);
+        if (corpsePool.Count == 0) return;
+
+        GameObject oldestCorpse = corpsePool.Dequeue();
+
+        oldestCorpse.transform.position = position;
+        oldestCorpse.SetActive(true);
+
+        corpsePool.Enqueue(oldestCorpse);
     }
 }
--- Assets/Scripts/HealthScripts/PlayerHealth.cs
+++ Assets/Scripts/HealthScripts/PlayerHealth.cs
@@ -7,5 +7,6 @@ public class PlayerHealth : Health
         base.Die();
         // Tambahkan logika khusus untuk kematian pemain di sini, seperti memicu animasi kematian atau menampilkan layar game over.
         Debug.Log("Player has died.");
+        GameplayManager.Instance.HandlePlayerDeath();
     }
 }
